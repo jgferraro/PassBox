59a193422f502c034633d4f0b4029a1d95ac6894
= <JonPecoraro@gmail.com>
2014-01-16 20:34:59 -0500

    Added default settings

--- js/passBox.js
+++ js/passBox.js
@@ -1,9 +1,14 @@
 (function($) {
-	$.fn.mask = function() {
+	$.fn.mask = function(options) {
+		
+		var settings = $.extend({
+			duration: 2000 // Show character for 2 seconds by default
+		}, options);
+		
 		this.each(function() { // loop through all matching selectors 
-
+			
 			// plugin code here
-
+			
 		});
 	}
 }(jQuery));
