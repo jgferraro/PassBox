0ff031e72a6d38e7b85be9912ca131e5e403057a
= <JonPecoraro@gmail.com>
2014-01-16 20:49:37 -0500

    Added default character setting

--- js/passBox.js
+++ js/passBox.js
@@ -2,7 +2,8 @@
 	$.fn.mask = function(options) {
 		
 		var settings = $.extend({
-			duration: 2000 // Show character for 2 seconds by default
+			duration: 		2000, 		// Show plain-text character for 2 seconds by default
+			maskCharacter:  '\u25CF' 	// Bullet character
 		}, options);
 		
 		this.each(function() { // loop through all matching selectors 
