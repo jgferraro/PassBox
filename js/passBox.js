bea33a9d287d2a6a09fe2cf61b1b581022c3740a
= <JonPecoraro@gmail.com>
2014-01-16 22:04:48 -0500

    Modified demo to include case where no input type is specified. Added code to core function to set password and text fields.

--- js/passBox.js
+++ js/passBox.js
@@ -1,15 +1,48 @@
 (function($) {
 	$.fn.mask = function(options) {
-		
 		var settings = $.extend({
 			duration: 		2000, 		// Show plain-text character for 2 seconds by default
 			maskCharacter:  '\u25CF' 	// Bullet character
 		}, options);
 		
 		this.each(function() { // loop through all matching selectors 
+			var $this = $(this);
+			var $textField;
+			var $passwordField;
 			
-			// plugin code here
+			// Set password and text fields
+			if ($this.attr('type') === 'password') {
+				$passwordField = $this;
+				$textField = $('<input type="text" />');
+			}
+			else {
+				$passwordField = $('<input type="password" />').attr('name', $this.attr('name'));
+				$textField = $this.removeAttr('name');
+			}
 			
+			// Add event listeners
+			$textField.on('keyup', onKeyUp);
+			$textField.on('change', onChange);
 		});
+		
+		function maskInput() {
+			// TODO: implement function
+		}
+		
+		function onKeyUp(e) {
+			// TODO: implement function
+		}
+		
+		function onChange(e) {
+			// TODO: implement function
+		}
+		
+		function isValidCharacter(character) {
+			// TODO: implement function
+		}
+		
+		function getCursorPosition() {
+			// TODO: implement function
+		}
 	}
 }(jQuery));
