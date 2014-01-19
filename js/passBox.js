6dd178d24f771349d102cbedfdc833d4e62c0039
= <JonPecoraro@gmail.com>
2014-01-19 15:18:56 -0500

    - Updated demo with a scenario where the input is given with the wrong type - Handled situation when the given input is not of the correct type - Changed text elements to hold a reference to associated password element as a data attribute. - Implemented timer functionality. - onKeyDown() and onChange() funcitons should be complete. - Added basic password making

--- js/passBox.js
+++ js/passBox.js
@@ -4,37 +4,67 @@
 			duration:		2000,		// Show plain-text character for 2 seconds by default
 			maskCharacter:	'\u25CF'	// Bullet character
 		}, options);
+
+		// Set global timer variable
+		var timer = null;
 		
 		this.each(function() { // Loop through all matching selectors 
 			var $this = $(this);
 			var $textField;
-			var $passwordField;
-			
+
 			// Set password and text fields
+			// Text fields will hold a reference to their associated password element as a data attribute
 			if ($this.prop('type') === 'password') {
-				$passwordField = $this;
-				$textField = $('<input type="text" />');
+				$textField = $('<input type="text" />').data('passwordField', $this);
+				$this.after($textField).hide();
 			}
 			else if ($this.prop('type') === 'text') {
-				$passwordField = $('<input type="password" />').attr('name', $this.attr('name'));
-				$textField = $this.removeAttr('name');
+				$textField = $this;
+				$passwordField = $this.clone().removeAttr('id').attr('type', 'password').hide();
+				$textField.data('passwordField', $passwordField).removeAttr('name').after($passwordField);
+			}
+			else {
+				return; // Input is not of the correct type; exit gracefully
 			}
 			
 			// Add event listeners
 			$textField.on('keyup', onKeyUp);
+			$textField.on('keydown', onKeyDown);
 			$textField.on('change', onChange);
 		});
 		
-		function maskInput() {
-			// TODO: implement function
+		/*
+			params:
+				target: The text input element to mask
+				operation: The operation that was executed: add, remove, or null
+		*/
+		function maskInput(target, operation) {
+			var $textField = $(target);
+			var $passwordFiled = $textField.data('passwordField');
+			var textValue = $textField.val();
+			var regExpMask = new RegExp('[^' + settings.maskCharacter + ']', 'g'); // using RegExp for IE 7 compatibility
+
+			if  (operation == null) {
+				$textField.val(textValue.replace(regExpMask, settings.maskCharacter));
+			}
 		}
 		
 		function onKeyUp(e) {
-			// TODO: implement function
+			var $this = $(this);
+			var operation = null;
+			startTimer(function() {
+				maskInput($this, operation);
+			});
+		}
+
+		function onKeyDown(e) {
+			stopTimer();
+			maskInput(this);
 		}
 		
 		function onChange(e) {
-			// TODO: implement function
+			stopTimer();
+			maskInput(this);
 		}
 		
 		function isValidCharacter(character) {
@@ -43,6 +73,14 @@
 		
 		function getCursorPosition() {
 			// TODO: implement function
+		}
+
+		function startTimer(func) {
+			timer = setTimeout(func, settings.duration);
+		}
+
+		function stopTimer() {
+			clearTimeout(timer);
 		}
 	}
 }(jQuery));
